4db2cd32c4b2145ad03829f6a43be643c46a975f
Johnwil Nicer <jnicer@clarity-mcs.com>
2025-08-11 20:59:13 +0000

    Copy the exact design I uploaded

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,19 +10,7 @@ export function Logo({ className }: { className?: string }) {
     >
       <rect width="256" height="256" fill="none" />
       <path
-        d="M40,88.00005,128,40.00005l88,48v64.00012c0,35.50526-27.36212,65.35249-62.03119,77.90158L128,216.00005l-25.96881-6.0983c-34.66907-12.54909-62.03119-42.39632-62.03119-77.90158Z"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="16"
-      />
-      <polyline
-        points="172 104 112 164 84 136"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="16"
+        d="M40,88.00005,128,40.00005l88,48v64.00012c0,35.50526-27.36212,65.35249-62.03119,77.90158L128,216.00005l-25.96881-6.0983c-34.66907-12.54909-62.03119-42.39632-62.03119-77.90158Z M172,104l-60,60-28-28,12-12,16,16,48-48Z"
       />
     </svg>
   );
