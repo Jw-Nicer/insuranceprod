66da3663269d31357ad4ed6601902b0c0a3d3d15
Johnwil Nicer <jnicer@clarity-mcs.com>
2025-08-11 20:10:49 +0000

    Change the logo, I want it to reflect Insurance Assistance

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -14,7 +14,15 @@ export function Logo({ className }: { className?: string }) {
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="24"
+        strokeWidth="16"
+      />
+      <path
+        d="M164.4,120a32,32,0,0,1-52.8,22.4L104,134.4a32,32,0,0,1-52.8-22.4,32,32,0,0,1,64-22.4"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
       />
     </svg>
   );
