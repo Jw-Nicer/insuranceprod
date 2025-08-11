ce256158d049e82afcc4620cfc9d8910c9d10f99
Johnwil Nicer <jnicer@clarity-mcs.com>
2025-08-11 20:59:47 +0000

    Change the logo to a shield but not filled color

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,12 +6,13 @@ export function Logo({ className }: { className?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 256"
       className={cn("h-6 w-6", className)}
-      fill="currentColor"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="16"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
-      <rect width="256" height="256" fill="none" />
-      <path
-        d="M40,88.00005,128,40.00005l88,48v64.00012c0,35.50526-27.36212,65.35249-62.03119,77.90158L128,216.00005l-25.96881-6.0983c-34.66907-12.54909-62.03119-42.39632-62.03119-77.90158Z M172,104l-60,60-28-28,12-12,16,16,48-48Z"
-      />
+      <path d="M40 88L128 40l88 48v64c0 35.5-27.4 65.4-62 77.9L128 216l-26-6.1c-34.6-12.5-62-42.4-62-77.9V88z" />
     </svg>
   );
 }
