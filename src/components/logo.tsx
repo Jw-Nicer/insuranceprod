22e78aa28fd7ec7affba2ac02fc88c42d0c1db9e
Johnwil Nicer <jnicer@clarity-mcs.com>
2025-08-11 20:56:44 +0000

    I want to logo to be full shape shield

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,15 +10,6 @@ export function Logo({ className }: { className?: string }) {
       <rect width="256" height="256" fill="none" />
       <path
         d="M40,88.00005,128,40.00005l88,48v64.00012c0,35.50526-27.36212,65.35249-62.03119,77.90158L128,216.00005l-25.96881-6.0983c-34.66907-12.54909-62.03119-42.39632-62.03119-77.90158Z"
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="16"
-      />
-      <polyline
-        points="172 104 113.333 160 84 132"
-        fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
