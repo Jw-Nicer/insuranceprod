1e5c63fd8a5f0f498d10b11415a0526cd913f47d
Johnwil Nicer <jnicer@clarity-mcs.com>
2025-08-11 20:57:24 +0000

    I want the logo to shape like this:

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,10 +6,19 @@ export function Logo({ className }: { className?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 256 256"
       className={cn("h-6 w-6", className)}
+      fill="currentColor"
     >
       <rect width="256" height="256" fill="none" />
       <path
         d="M40,88.00005,128,40.00005l88,48v64.00012c0,35.50526-27.36212,65.35249-62.03119,77.90158L128,216.00005l-25.96881-6.0983c-34.66907-12.54909-62.03119-42.39632-62.03119-77.90158Z"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="16"
+      />
+      <polyline
+        points="172 104 112 164 84 136"
+        fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
